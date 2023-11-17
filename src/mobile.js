e6f31fefbae910be1b6baf10c1fb65078dc8076f
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-17 15:33:31 +0530

    Initial commit

---
+++ src/mobile.js
@@ -0,0 +1,15 @@
+// Progression 1 SmartPhone
+
+class SmartPhone {}
+
+// Progression 2 AndroidPhone
+
+class AndroidPhone extends SmartPhone {}
+
+// Progression 3 IPhone
+
+class IPhone extends SmartPhone {}
+
+// Progression 4  Progression 1 MobileCampus
+
+class MobileCampus {}
